ed10e4fbc3324dde8fa2a160b041a87079d48939
Eden Hu <edenhu11@gmail.com>
2019-07-31 13:59:35 -0400

    added model select fn

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -6,3 +6,17 @@ generate_data <- function(n, p) {
   list(covariates = mat, responses = rnorm(n))
 }
 
+model_select <- function(covariates, responses, cutoff) {
+  fit1 <- lm(responses ~ covariates[,c(1, ncol(covariates))])
+  bool_ind <- ((summary(fit1)$coefficients[,4]) <= cutoff)
+  bool_ind <- as.vector(bool_ind)
+  if (all(bool_ind == FALSE)) {
+    return (vector())
+  }
+  mat2 <- covariates[,bool_ind]
+  fit2 <- lm(responses ~ mat2)
+  res <- (summary(fit2)$coefficients[,4][2])
+  res <- as.vector(res)
+  res
+}
+
